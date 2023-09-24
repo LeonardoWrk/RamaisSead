eadfa9f63dc97a0eeaaac1552e8bbc0b64b1974a
bmwork <brenomartinsmb@gmail.com>
2023-09-24 17:16:25 -0300

    initoption

---
+++ initOption.cjs
@@ -0,0 +1,84 @@
+const fs = require('fs');
+const path = require('path');
+
+// INIT OPTION
+
+const dataToWrite = {
+	setor: [
+		'',
+		'SGRH',
+		'DAF',
+		'ASCOM',
+		'CPSIAD',
+		'ALMOXARIFADO',
+		'GERPS',
+		'GERFIN',
+		'GERATA',
+		'SGCC',
+		'OUVIDORIA',
+		'PERICIA MEDICA',
+		'SETRANS',
+		'DGSL',
+		'GERMACO',
+		'GERSEP',
+		'GERMASE',
+		'GERAFOR',
+		'GABINETE',
+		'RECEPÇÃO',
+		'GECIF',
+		'CONCURSO',
+		'INDENIZACAO',
+		'GCSA',
+		'DGP',
+		'GCDFF',
+		'INCORPORAÇÃO',
+		'GELOT',
+		'ASPOCT',
+		'DGPI',
+		'SUPAT',
+		'DGPM',
+		'GEMOVE',
+		'DGP',
+		'CONTABILIDADE',
+		'NÚCLEO',
+		'GUARITA',
+		'SERVICO SOCIAL',
+		'SECLOG',
+		'DTIN',
+		'ADESP',
+		'DTI-TURISTA',
+		'ACUMULO DE CARGOS',
+		'PROTOCOLO',
+		'ESCOLA',
+		'GCLOG'
+	],
+	unidade: [
+		'',
+		'CONTABILIDADE',
+		'NÚCLEO',
+		'GUARITA',
+		'SERVICO SOCIAL',
+		'SECLOG',
+		'DTIN',
+		'ADESP',
+		'DTI-TURISTA',
+		'ACUMULO DE CARGOS',
+		'PROTOCOLO',
+		'ESCOLA',
+		'GCLOG'
+	]
+};
+
+const directoryPath = './';
+const fileName = 'option.json';
+
+const filePath = path.join(directoryPath, fileName);
+const jsonData = JSON.stringify(dataToWrite, null, 2);
+
+fs.writeFile(filePath, jsonData, 'utf8', (err) => {
+	if (err) {
+		console.error('Error writing JSON file:', err);
+	} else {
+		console.log('JSON file written successfully!');
+	}
+});
