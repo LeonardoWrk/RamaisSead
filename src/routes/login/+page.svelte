809c797f130c4cb16eca5d0ffd4aa09bc202591f
LeonardoWrk <leocardozo0704@gmail.com>
2023-12-05 10:09:20 -0300

    mais ui changes

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -21,16 +21,16 @@
 	}
 </script>
 
-<div class="w-full flex justify-center mt-40 text-white drop-shadow-lg">
+<div class="w-full flex justify-center mt-40 text-black drop-shadow-lg">
 	<form
 		on:submit|preventDefault={handlelogin}
-		class="bg-[#1a1c26] flex flex-col w-1/5 rounded-t-lg drop-shadow-lg"
+		class="bg-[white] flex flex-col w-1/5 rounded-t-lg shadow-lg shadow-[#002776] opacity-75"
 		action=""
 	>
-		<h2 class="text-white text-center bg-theme-soft h-[%80] rounded-t-lg p-2 text-xl">login</h2>
+		<h2 class="text-black text-center bg-gray-300 h-[%80] rounded-t-lg p-2 text-xl">login</h2>
 		<div class="flex flex-col mt-10 items-center">
 			<div class="w-[80%] flex flex-col">
-				<label class="text-white mb-2" for="username">Usuario</label>
+				<label class="text-black mb-2" for="username">Usuario</label>
 				<input
 					class=" h-8 rounded-lg bg-theme-secondaryBase text-white border border-[#27293a] text-2xl"
 					id="user"
@@ -40,7 +40,7 @@
 			</div>
 
 			<div class="w-[80%] flex flex-col mt-10">
-				<label class="text-white mb-2" for="username">Senha</label>
+				<label class="text-black mb-2" for="username">Senha</label>
 				<input
 					class=" h-8 rounded-lg bg-theme-secondaryBase text-white border border-[#27293a] text-2xl"
 					id="pass"
@@ -50,7 +50,7 @@
 			</div>
 			<button
 				type="submit"
-				class=" p-2 m-10 w-2/5 bg-theme-soft rounded-md active:scale-90 duration-100"
+				class=" p-2 m-10 w-2/5 bg-gray-300 rounded-md active:scale-90 duration-100"
 			>
 				Logar
 			</button>
